9e58fffdcac829fedb9768ac1cf5021c128ce62c
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-02-16 09:15:14 -0500

    ast node

--- src/main/kotlin/arithlang/AST.kt
+++ src/main/kotlin/arithlang/AST.kt
@@ -14,19 +14,45 @@ interface AST {
 
         abstract class Exp: ASTNode(){}
 
-//        abstract class NumExp(val v: Double): Exp() {
-//            override fun <T> accept(visitor: Visitor<T>): T {
-//                return visitor.visit(this)
-//            }
-//        }
+        abstract class NumExp(val v: Double): Exp() {
+            override fun <T> accept(visitor: Visitor<T>): T {
+                return visitor.visit(this)
+            }
+        }
+
+        abstract class CompoundArithExp(vararg args: Exp): Exp() {}
+
+        abstract class AddExp(vararg args: Exp): CompoundArithExp(){
+            override fun <T> accept(visitor: Visitor<T>): T {
+                return visitor.visit(this)
+            }
+        }
+
+        abstract class SubExp(vararg args: Exp): CompoundArithExp(){
+            override fun <T> accept(visitor: Visitor<T>): T {
+                return visitor.visit(this)
+            }
+        }
+
+        abstract class MultExp(vararg args: Exp): CompoundArithExp(){
+            override fun <T> accept(visitor: Visitor<T>): T {
+                return visitor.visit(this)
+            }
+        }
+
+        abstract class DivExp(vararg args: Exp): CompoundArithExp(){
+            override fun <T> accept(visitor: Visitor<T>): T {
+                return visitor.visit(this)
+            }
+        }
 
         interface Visitor<T> {
             fun visit(p: Program): T
-//            fun visit(): T
-//            fun visit(): T
-//            fun visit(): T
-//            fun visit(): T
-
+            fun visit(e: NumExp): T
+            fun visit(e: AddExp): T
+            fun visit(e: SubExp): T
+            fun visit(e: MultExp): T
+            fun visit(e: DivExp): T
         }
     }
 }
