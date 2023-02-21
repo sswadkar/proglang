a9e3d4c3177820a6bb4a8ddc0aa500f25f1aeae1
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-02-20 22:02:44 -0500

    complete parser

--- src/main/kotlin/arithlang/Node.kt
+++ src/main/kotlin/arithlang/Node.kt
@@ -1,7 +1,10 @@
 package arithlang
 
+import arithlang.lexer.Token
 import arithlang.lexer.TokenType
 
 sealed class Node {
-    class Exp(val symbol: TokenType, vararg nodes: Node): Node() {}
+    // using varargs allows us to create compounded arithmetic expressions
+    class CompoundExp(val symbol: TokenType, vararg nodes: Node): Node() {}
+    class UnaryNode(val type: TokenType, val value: Token): Node() {}
 }
--- src/main/kotlin/arithlang/Parser.kt
+++ src/main/kotlin/arithlang/Parser.kt
@@ -1,4 +1,46 @@
 package arithlang
 
-class Parser() {
+import arithlang.lexer.Token
+import arithlang.lexer.TokenType
+
+class Parser(private val tokens: MutableList<Token>) {
+    private val tokenIterator = tokens.listIterator()
+
+    private var currentSymbol: TokenType = TokenType.UNDEFINED
+
+    fun parse(): List<Node> {
+        val nodes = mutableListOf<Node>()
+
+        while (tokenIterator.hasNext()) {
+            val nextSymbol = tokenIterator.next()
+
+            if (isOperator(nextSymbol.tokenType)){
+                currentSymbol = nextSymbol.tokenType
+                nodes.add(extractExpression())
+            }
+        }
+
+        return nodes
+    }
+
+    private fun lookAhead(): Token?{
+        return tokens[tokenIterator.nextIndex()]
+    }
+
+    private fun extractExpression(): Node {
+        var lhs : Node = Node.UnaryNode(TokenType.INT, tokenIterator.next())
+        val nextToken = lookAhead()
+
+        while (nextToken?.tokenType == TokenType.INT){
+            val recursiveNodeConstruction = extractExpression()
+
+            lhs = Node.CompoundExp(currentSymbol, lhs, recursiveNodeConstruction)
+        }
+
+        return lhs
+    }
+
+    private fun isOperator(symbol: TokenType): Boolean{
+        return symbol == TokenType.PLUS || symbol == TokenType.MINUS || symbol == TokenType.MULT || symbol == TokenType.DIV
+    }
 }
