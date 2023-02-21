e8292d34a5e60206fe5399072eb0720c0e734837
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-02-20 21:04:58 -0500

    switch node implementation to use sealed class

--- src/main/kotlin/arithlang/Node.kt
+++ src/main/kotlin/arithlang/Node.kt
@@ -1,6 +1,6 @@
 package arithlang
 
-interface Node {
-    data class TokenNode(val name: String, val children: MutableList<Node>): Node {}
-    data class ValueNode(val name: String, val children: MutableList<Node>): Node {}
+sealed class Node {
+    data class TokenNode(val name: String, val children: MutableList<Node>): Node() {}
+    data class ValueNode(val name: String, val children: MutableList<Node>): Node() {}
 }
