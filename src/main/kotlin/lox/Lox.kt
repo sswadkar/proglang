e9c16cb73b8547c7a58310b2b2209689ad7f28a4
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-03-08 08:57:39 -0500

    handle run configs

--- src/main/kotlin/lox/Lox.kt
+++ src/main/kotlin/lox/Lox.kt
@@ -1,7 +1,35 @@
 package lox
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.Scanner
+
 class Lox {
     companion object{
-        fun main() {}
+        fun runFile(path: String){
+            val bytes = Files.readAllBytes(Paths.get(path))
+            run(String(bytes, Charset.defaultCharset()))
+        }
+
+        fun runPrompt(){
+            val input = InputStreamReader(System.`in`)
+            val reader = BufferedReader(input)
+
+            while (true){
+                println("> ")
+                val line = reader.readLine() ?: break
+                run(line)
+            }
+        }
+
+        fun run(source: String){
+            val scanner = Scanner(source)
+            for (token in scanner.tokens()){
+                println(token)
+            }
+        }
     }
 }
